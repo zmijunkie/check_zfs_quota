10d401759f04b992c2d66bc806338a9dd8567852
Sascha ZMIJunkie Gresk <sascha.gresk@opensource-consult.com>
2018-08-10 15:13:23 +0200

    list_jail_quota.sh contains support for linux

--- list_jail_quota.sh
+++ list_jail_quota.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-zfs list -H -o name,used,avail,quota | grep jail | grep -v ezjail | grep 'rpool/jails/'
+INPUT_STRING=`uname`
+
+case $INPUT_STRING in
+    FreeBSD)
+        zfs list -H -o name,used,avail,quota | grep jail | grep -v ezjail | grep 'rpool/jails/'
+        ;;
+    Linux)
+        zfs list -H -o name,used,avail,quota | tail -n +2 
+        break
+        ;;
+    *)
+        echo "This script does not support your platform."
+        ;;
+esac
+
+# 
